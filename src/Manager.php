0326de2eaf6016f19ecb1fa0e7d6a696b85897fe
cjy <cjy632258@hotmail.com>
2019-01-14 09:26:14 +0800

    update

--- src/Manager.php
+++ src/Manager.php
@@ -30,21 +30,26 @@ class Manager
 
     /**
      * @param null $driver
+     * @param array $options
      * @return GatewayApplicationInterface
      */
-    public function uses($driver = null)
+    public function uses($driver = null, $options = [])
     {
-        return $this->getDriver($driver ?: $this->config['default']);
+        return $this->getDriver($driver ?: $this->config['default'], $options);
     }
 
     /**
      * @param $name
+     * @param array $options
      * @return GatewayApplicationInterface
      */
-    protected function getDriver($name)
+    protected function getDriver($name, $options = [])
     {
         if (array_key_exists($name, static::$drivers)) {
-            return call_user_func(static::$drivers[$name], array_get($this->config, 'drivers.' . $name));
+            $config = array_get($this->config, 'drivers.' . $name, []);
+
+            $config = array_merge($config, $options);
+            return call_user_func(static::$drivers[$name], $config);
         }
 
         throw new UnsupportedDriverException("driver [$name] not registered");
